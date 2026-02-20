5c4b89bb28ec615922f99c5dbdc5c7361b6c7af6
gercova <germancotrina17@gmail.com>
2026-02-20 00:57:32 -0500

    refactor: implementación de mensajes para el tipo de formato valido coomo jpeg, png, jpg, gif

--- app/Http/Requests/CourseValidate.php
+++ app/Http/Requests/CourseValidate.php
@@ -58,6 +58,7 @@ class CourseValidate extends FormRequest {
             'what_you_learn.*.string'   => 'Cada ítem a aprender debe ser una cadena de texto.',
             'image.image'               => 'El archivo ingresado debe ser una imagen válida.',
             'image.max'                 => 'El archivo no puede pesar más de 5MB.',
+            'image.mimes'               => 'El archivo debe ser jpeg, png, jpg o gif',
 
         ];
     }

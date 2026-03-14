a338ad08c41997f9e6f288f8dd4e6f34376b2159
gercova <germancotrina17@gmail.com>
2026-03-14 02:51:41 -0500

    feat: implementación de middleware CheckMaintenance

---
+++ app/Http/Middleware/CheckMaintenanceMode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckMaintenanceMode
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response {
+        if (env('APP_MAINTENANCE', false) && !$request->user()?->hasRole('admin')) {
+            return response()->view('errors.503', [], 503);
+        }
+        
+        return $next($request);
+    }
+}

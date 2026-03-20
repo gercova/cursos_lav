85b530572d3cecdb5f8fe0bb2f0e81809676095e
gercova <germancotrina17@gmail.com>
2026-03-19 23:20:55 -0500

    refactor: sin cambios relevantes

--- app/Models/CourseSale.php
+++ app/Models/CourseSale.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class CourseSale extends Model
-{
+class CourseSale extends Model {
     use HasFactory;
     protected $table        = 'course_sales';
     protected $primaryKey   = 'id';

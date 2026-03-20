ab6a0ac2800f503fff0ed387a61c83c6d5b3aee1
gercova <germancotrina17@gmail.com>
2026-03-19 23:23:01 -0500

    refactor: ruta de admin.users.sales implementada

--- routes/web.php
+++ routes/web.php
@@ -242,6 +242,7 @@ Route::prefix('admin')->group(function () {
         Route::put('/users/create-code/{user}',     [UserAdminController::class, 'createCode'])->name('admin.user.create-code');
         Route::put('/users/policy/{user}',          [UserAdminController::class, 'createLimitUser'])->name('admin.user.policy');
         Route::get('/users/get-policy/{user}',      [UserAdminController::class, 'getLimitUser'])->name('admin.user.get-policy');
+        Route::get('users/{user}/sales',            [UserAdminController::class, 'getSales'])->name('admin.users.sales');
 
         // Rutas para asignar permisos a usuarios
         Route::get('/roles/home',               [RoleController::class, 'index'])->name('admin.roles.index');

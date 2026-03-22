88efd299531990d10c8237ef9c86f54c583dc3f8
gercova <germancotrina17@gmail.com>
2026-03-22 16:16:28 -0500

    refactor: revalidación de la clase entera

--- app/Http/Requests/StaffValidate.php
+++ app/Http/Requests/StaffValidate.php
@@ -6,43 +6,77 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StaffValidate extends FormRequest {
 
+    // public function authorize(): bool {
+    //     return true;
+    // }
+
+    // public function rules(): array {
+    //     return [
+    //         'dni'           => 'required|string|max:20|unique:users',
+    //         'names'         => 'required|string|max:255',
+    //         'email'         => 'required|string|email|max:255|unique:users',
+    //         'password'      => 'required|string|min:8|confirmed',
+    //         'country_code'  => 'required|string|max:5',
+    //         'phone'         => 'required|string|max:20',
+    //         'nationality'   => 'required|string|max:100',
+    //         'address'       => 'nullable',
+    //         'profession'    => 'required|string|max:255',
+    //     ];
+    // }
+
+    // public function messages(): array {
+    //     return [
+    //         'dni.required'          => 'El DNI es obligatorio',
+    //         'dni.max'               => 'El DNI tiene una longitud máxima de 20 dígitos',
+    //         'dni.unique'            => 'El DNI ya existe',
+    //         'names.required'        => 'Los Nombres son obligatorios',
+    //         'names.max'             => 'Los Nombres tienen una longitud máxima de 255 caracteres',
+    //         'email.required'        => 'El email es requerido',
+    //         'email.email'           => 'El formato ingresado no corresponde al de un correo',
+    //         'email.unique'          => 'El email ya está registrado',
+    //         'phone.required'        => 'El teléfono es requerido',
+    //         'phone.max'             => 'El teléfono tiene una longitud máxima de 20 dígitos',
+    //         'nationality.required'  => 'La nacionalidad es olbigatoria',
+    //         'nationality.max'       => 'La nacionalidad tiene una longitud máxima de 100 caracteres',
+    //         // 'address.max'           => 'La dirección tiene una longitud máxima de 500 caracteres',
+    //         'profession.max'        => 'La profesión tiene una longitud máxima de 500 caracteres',
+    //         'password.required'     => 'La contraseña es requerida',
+    //         'password.min'          => 'La contraseña tiene una longitud mínima de 8 caracteres',
+    //         'password.confirmed'    => 'Debe confirmar la contraseña',
+    //         'country_code.required' => 'El código de país es obligatorio',
+    //     ];
+    // }
     public function authorize(): bool {
         return true;
     }
-
+ 
     public function rules(): array {
         return [
-            'dni'           => 'required|string|max:20|unique:users',
+            'dni'           => 'required|string|max:20|unique:users,dni,' . $this->input('user_id'),
             'names'         => 'required|string|max:255',
-            'email'         => 'required|string|email|max:255|unique:users',
-            'password'      => 'required|string|min:8|confirmed',
+            'email'         => 'required|string|email|max:255|unique:users,email,' . $this->input('user_id'),
             'country_code'  => 'required|string|max:5',
-            'phone'         => 'required|string|max:20',
-            'nationality'   => 'required|string|max:100',
-            'address'       => 'required|string|max:500',
-            'profession'    => 'required|string|max:255',
+            'phone'         => 'nullable|string|max:20',
+            'nationality'   => 'nullable|string|max:100',
+            'address'       => 'nullable|string|max:500',
+            'profession'    => 'nullable|string|max:255',
         ];
     }
-
+ 
     public function messages(): array {
         return [
             'dni.required'          => 'El DNI es obligatorio',
             'dni.max'               => 'El DNI tiene una longitud máxima de 20 dígitos',
-            'dni.unique'            => 'El DNI ya existe',
-            'names.required'        => 'Los Nombres son obligatorios',
-            'names.max'             => 'Los Nombres tienen una longitud máxima de 255 caracteres',
+            'dni.unique'            => 'El DNI ya está registrado',
+            'names.required'        => 'Los nombres son obligatorios',
+            'names.max'             => 'Los nombres tienen una longitud máxima de 255 caracteres',
             'email.required'        => 'El email es requerido',
             'email.email'           => 'El formato ingresado no corresponde al de un correo',
             'email.unique'          => 'El email ya está registrado',
-            'phone.required'        => 'El teléfono es requerido',
             'phone.max'             => 'El teléfono tiene una longitud máxima de 20 dígitos',
-            'nationality.required'  => 'La nacionalidad es olbigatoria',
             'nationality.max'       => 'La nacionalidad tiene una longitud máxima de 100 caracteres',
             'address.max'           => 'La dirección tiene una longitud máxima de 500 caracteres',
-            'profession.max'        => 'La profesión tiene una longitud máxima de 500 caracteres',
-            'password.required'     => 'La contraseña es requerida',
-            'password.min'          => 'La contraseña tiene una longitud mínima de 8 caracteres',
-            'password.confirmed'    => 'Debe confirmar la contraseña',
+            'profession.max'        => 'La profesión tiene una longitud máxima de 255 caracteres',
             'country_code.required' => 'El código de país es obligatorio',
         ];
     }

ca5c708c23d4b31ac857dafde066b04f1bffd42c
gercova <germancotrina17@gmail.com>
2026-03-23 21:09:02 -0500

    refactor: clase refactorizada

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -25,17 +25,10 @@ class ForgotPasswordController extends Controller
     use SendsPasswordResetEmails;
 
     public function showLinkRequestForm(): View {
-        // Obtener la información de la empresa (si está disponible)
         $enterprise = Enterprise::first();
         return view('auth.forgot-password', compact('enterprise'));
     }
 
-    /**
-     * Send a reset link to the given user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
     public function sendResetLinkEmail(Request $request) {
         $this->validateEmail($request);
 
@@ -43,30 +36,22 @@ class ForgotPasswordController extends Controller
             $request->only('email')
         );
 
-        return $response == Password::RESET_LINK_SENT
-                    ? $this->sendResetLinkResponse($request, $response)
-                    : $this->sendResetLinkFailedResponse($request, $response);
+        return $response == Password::RESET_LINK_SENT ? $this->sendResetLinkResponse($request, $response) : $this->sendResetLinkFailedResponse($request, $response);
     }
 
-    /**
-     * Get the response for a successful password reset link.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string  $response
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
     protected function sendResetLinkResponse(Request $request, $response) {
         return back()->with('status', trans($response));
     }
 
-    /**
-     * Get the response for a failed password reset link.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string  $response
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
     protected function sendResetLinkFailedResponse(Request $request, $response) {
         return back()->withInput($request->only('email'))->withErrors(['email' => trans($response)]);
+    }
+
+    // Aquí va la validación correcta papu
+    protected function validateEmail(Request $request) {
+        $request->validate(
+            ['email' => 'required|email|exists:users,email'],
+            ['email.exists' => 'No encontramos ninguna cuenta registrada con este correo electrónico.']
+        );
     }
 }

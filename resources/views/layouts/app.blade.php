ba1c6607ebbf3d9688bedbbcde896d956c3d5568
gercova <germancotrina17@gmail.com>
2026-07-24 18:23:08 -0500

    feat:  link de font-awesome removido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,6 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/bootstrap-icons/font/bootstrap-icons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/font-awesome.all.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="{{ asset('js/tailwindcss.js') }}"></script>
     <script src="{{ asset('js/alpine.js') }}" defer></script>
